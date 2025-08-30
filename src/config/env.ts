ce95567c7c6bac9800c47e6c9953be0dd6a7bf29
Arthur Lopes <arthurlopescontact@gmail.com>
2025-08-30 13:58:12 -0300

    feat: remove INSTANCE_ID from environment configuration and update DNS handler to use instance ID from event

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,7 +1,6 @@
 import z from 'zod';
 
 export const schema = z.object({
-  INSTANCE_ID: z.string().min(1, 'INSTANCE_ID is required'),
   HOSTED_ZONE_ID: z.string().min(1, 'HOSTED_ZONE_ID is required'),
   RECORD_NAME: z.string().min(1, 'RECORD_NAME is required'),
 });
--- src/functions/dns.ts
+++ src/functions/dns.ts
@@ -1,10 +1,36 @@
-import { env } from '../config/env';
-import { getInstancePublicIp } from '../lib/ec2';
-import { updateDnsRecord } from '../lib/route53';
+import { EventBridgeEvent } from "aws-lambda";
+import { env } from "../config/env";
+import { getInstancePublicIp } from "../lib/ec2";
+import { updateDnsRecord } from "../lib/route53";
 
-export async function handler() {
+type Ec2StateChangeDetail = {
+  "instance-id": string;
+  state:
+    | "pending"
+    | "running"
+    | "stopping"
+    | "stopped"
+    | "shutting-down"
+    | "terminated";
+};
+
+export async function handler(
+  event: EventBridgeEvent<
+    "EC2 Instance State-change Notification",
+    Ec2StateChangeDetail
+  >
+) {
   try {
-    const publicIpAddress = await getInstancePublicIp({ instanceId: env.INSTANCE_ID });
+    const instanceId = event.detail["instance-id"];
+
+    if (!instanceId) {
+      console.error("No instance ID found in event payload.");
+      return;
+    }
+
+    const publicIpAddress = await getInstancePublicIp({
+      instanceId: instanceId,
+    });
 
     if (!publicIpAddress) {
       console.log("Failed to retrieve public IP address.");
@@ -14,10 +40,10 @@ export async function handler() {
     await updateDnsRecord({
       hostedZoneId: env.HOSTED_ZONE_ID,
       recordName: env.RECORD_NAME,
-      ip: publicIpAddress
+      ip: publicIpAddress,
     });
     console.log(`DNS record updated with IP: ${publicIpAddress}`);
   } catch (error) {
     console.log("Error updating DNS record:", error);
   }
-};
+}
